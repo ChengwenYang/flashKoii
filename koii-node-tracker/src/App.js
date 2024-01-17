836b0d85a041f69017de13c18cf8fc8401589c2b
Trevor <ch807441@dal.ca>
2024-01-17 12:53:58 -0400

    firework

--- koii-node-tracker/src/App.js
+++ koii-node-tracker/src/App.js
@@ -1,39 +1,46 @@
 import React, { useEffect, useState } from 'react';
 import { Howl } from 'howler';
-import { Spin } from 'react-cssfx-loading';
+import axios from 'axios';
 import './App.css';
- // 确保正确导入 Spin 组件
 
 function App() {
-  const [isNewNode, setIsNewNode] = useState(false);
-  const [nodeCount, setNodeCount] = useState(0); // 定义 nodeCount 状态
-  const [showEffect, setShowEffect] = useState(false); // 定义 showEffect 状态
+  const [nodeCount, setNodeCount] = useState(0);
+  const [showEffect, setShowEffect] = useState(false);
+  const api = 'https://faucet-api.koii.network/api/leaderboard/get-nodes-summary';
 
   useEffect(() => {
-    const checkNewNode = () => {
-      const interval = setInterval(() => {
-        const newNodeDetected =null; // 随机示例
-        if (newNodeDetected) {
-          setIsNewNode(true);
-          setNodeCount(prevCount => prevCount + 1); // 更新节点计数
-          setShowEffect(true); // 显示特效
-          playSound();
+    let fetchNode = nodeCount;
 
-          setTimeout(() => setShowEffect(false), 3000); // 3秒后隐藏特效
+    const checkNewNode = async () => {
+      try {
+        const response = await axios.get(api);
+        const data = response.data;
+        const totalNodes = data.totalNumberOfActiveStakingKeys;
+
+        if (fetchNode !== 0 && totalNodes > fetchNode) {
+          setShowEffect(true);
+          playSound();
+          setTimeout(() => setShowEffect(false), 5000); 
         }
-      }, 5000);
-      return () => clearInterval(interval); // 清除定时器
+
+        setNodeCount(totalNodes);
+        fetchNode = totalNodes;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
     };
 
-    checkNewNode();
-  }, []);
+    const interval = setInterval(checkNewNode, 6000);
+    return () => clearInterval(interval);
+  }, [nodeCount]);
 
   const playSound = () => {
     const sound = new Howl({
-      src: ['sound.mp3'] // 替换为您的音效文件路径
+      src: ['sound.mp3'] 
     });
     sound.play();
   };
+
   function CelebrationEffect() {
     return (
       <div className="celebration">
